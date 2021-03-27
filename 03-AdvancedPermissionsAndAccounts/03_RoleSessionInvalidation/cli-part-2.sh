b2bd97d828de2c2f1e71acec28316db885b0399b
Sameer Jain <sameerjain.50@gmail.com>
2021-03-27 21:28:43 +1100

    Create cli-part-2.sh

---
+++ 03-AdvancedPermissionsAndAccounts/03_RoleSessionInvalidation/cli-part-2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#revoke session of role
+
+# dirty way to find instance role
+EC2RoleName=$(aws iam list-roles --query Roles[].RoleName | grep InstanceRole | cut -f2 -d "\"")
+
+#find physical ID of instances and assign to variables
+EC2InstaceA_ID=$(aws cloudformation describe-stack-resource --stack-name A4LHostingInc --logical-resource-id EC2InstanceA --query StackResourceDetail.PhysicalResourceId --output text)
+EC2InstaceB_ID=$(aws cloudformation describe-stack-resource --stack-name A4LHostingInc --logical-resource-id EC2InstanceB --query StackResourceDetail.PhysicalResourceId --output text)
+
+#find EC2 instance role association ID
+EC2InstaceA_AssociationID=$(aws ec2 describe-iam-instance-profile-associations --filters Name=instance-id,Values=[$EC2InstaceA_ID] --query IamInstanceProfileAssociations[].AssociationId --output text)
+EC2InstaceB_AssociationID=$(aws ec2 describe-iam-instance-profile-associations --filters Name=instance-id,Values=[$EC2InstaceB_ID] --query IamInstanceProfileAssociations[].AssociationId --output text)
+
+#find Instance profile name
+Instace_Profile_Arn=$(aws ec2 describe-iam-instance-profile-associations --filters Name=instance-id,Values=[$EC2InstaceA_ID] --query IamInstanceProfileAssociations[].IamInstanceProfile[].Arn --output text)
+
+#Instead of rebooting EC2 instance reassociate Instance Profile 
+aws ec2 disassociate-iam-instance-profile --association-id $EC2InstaceA_AssociationID
+aws ec2 associate-iam-instance-profile --instance-id $EC2InstaceA_ID --iam-instance-profile Arn=$Instace_Profile_Arn
+aws ec2 disassociate-iam-instance-profile --association-id $EC2InstaceB_AssociationID
+aws ec2 associate-iam-instance-profile --instance-id $EC2InstaceB_ID --iam-instance-profile Arn=$Instace_Profile_Arn
+
