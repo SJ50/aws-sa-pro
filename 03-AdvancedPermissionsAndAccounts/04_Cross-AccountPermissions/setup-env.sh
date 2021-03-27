73a4fb10ca8223de73d05a94aa44bb36378b8ea7
Sameer Jain <sameerjain.50@gmail.com>
2021-03-28 00:43:49 +1100

    Create setup-env.sh

---
+++ 03-AdvancedPermissionsAndAccounts/04_Cross-AccountPermissions/setup-env.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#  set cli mode to partial
+aws configure set cli_auto_prompt on-partial
+
+StackName=user-bob
+
+# create cloudformation stack
+echo "creating user bob"
+aws cloudformation create-stack --stack-name $StackName \
+    --template-body file://01_DEMOSETUP/bob.yaml \
+    --parameters ParameterKey=bobpassword,ParameterValue=boBpassword*1 \
+    --capabilities CAPABILITY_NAMED_IAM
+
+while  [ "$(aws cloudformation describe-stacks --stack-name $StackName --query Stacks[0].StackStatus --output text)" != "CREATE_COMPLETE" ]; do 
+  aws cloudformation describe-stacks --stack-name $StackName --query Stacks[0].StackStatus --output text
+  sleep 1
+done
+  
+MasterAccountID=$(aws sts get-caller-identity --query Account --output text)
+  
+#creating 3 S3 buckets and IAM Role to assume by Master Account in prod account
+echo ""
+echo "creating 3 S3 buckets in PROD Account"
+
+S3Buckets=Buckets
+aws cloudformation create-stack \
+    --stack-name $S3Buckets \
+    --template-body file://01_DEMOSETUP/prod_bucketsandrole.yaml \
+    --profile prod \
+    --parameters ParameterKey=AccountToTrust,ParameterValue=$MasterAccountID \
+    --capabilities CAPABILITY_IAM
+    
+while  [ "$(aws cloudformation describe-stacks --stack-name $S3Buckets --query Stacks[0].StackStatus --profile prod --output text)" != "CREATE_COMPLETE" ]; do 
+  aws cloudformation describe-stacks --stack-name $S3Buckets --query Stacks[0].StackStatus --profile prod --output text
+  sleep 1
+done    
+
+#find IAM Role name
+
+IAMRoleName=$(aws iam list-roles --profile prod --query 'Roles[?contains(RoleName, `Buckets`) == `true`]|[0].RoleName' --output text)
+echo "IAM Role Name = $IAMRoleName"
+echo ""
+PRODAccountID=$(aws sts get-caller-identity --profile prod --query Account --output text)
+  
+echo "Production Account ID = $PRODAccountID"
+  
+echo ""
+bucket1=$(aws s3 ls --profile prod | cut -f3 -d " " | awk 'NR==1')
+bucket2=$(aws s3 ls --profile prod | cut -f3 -d " " | awk 'NR==2')
+bucket3=$(aws s3 ls --profile prod | cut -f3 -d " " | awk 'NR==3')
+
+echo "bucket 1 url = https://console.aws.amazon.com/s3/buckets/$bucket1?region=us-east-1&tab=objects"
+echo "bucket 2 url = https://console.aws.amazon.com/s3/buckets/$bucket2?region=us-east-1&tab=objects"
+echo "bucket 3 url = https://console.aws.amazon.com/s3/buckets/$bucket3?region=us-east-1&tab=objects"
+
+AccountAliases=$(aws iam list-account-aliases --query AccountAliases --output text)
+echo "user url to sign in as bob user https://$AccountAliases.signin.aws.amazon.com/console/"
+BobUserName=$(aws iam list-users --query 'Users[?contains(UserName, `bob`) == `true`]|[0].UserName' --output text)  
+echo "username = $BobUserName"
+echo "password = boBpassword*1"
+MasterAccountCanonicalID=$(aws s3api list-buckets --query Owner.ID --output text)
+echo "Master Account Canonical_ID = $MasterAccountCanonicalID"
+
